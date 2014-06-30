3531116c871e2d1cedd4f03f70182ab55e62707e
apeeclgithub <apeeclgithub@gmail.com>
2014-06-30 19:42:05 -0400

    update registro

--- function/registro.php
+++ function/registro.php
@@ -1,10 +1,9 @@
 <?php
+	include 'conexion.php';
+
 if($_POST)
 {
-	$to_Email   	= "mario.meneses.a@gmail.com"; //Replace with recipient email address
-	$subject        = 'Tienes un e-mail de contacto desde Cannabis'; //Subject line for emails
-	
-	
+
 	//check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
 	
@@ -19,93 +18,65 @@ if($_POST)
     } 
 	
 	//check $_POST vars are set, exit if any missing
-	if(!isset($_POST["userName"]) || !isset($_POST["userEmail"]) || !isset($_POST["userPhone"]) || !isset($_POST["userMessage"]))
+	if(!isset($_POST["userName1"]) || !isset($_POST["userEmail1"]) || !isset($_POST["userPass1"]) || !isset($_POST["userDate1"]))
 	{
 		$output = json_encode(array('type'=>'error', 'text' => 'Campos vacios'));
 		die($output);
 	}
 
 	//Sanitize input data using PHP filter_var().
-	$user_Name        = filter_var($_POST["userName"], FILTER_SANITIZE_STRING);
-	$user_Email       = filter_var($_POST["userEmail"], FILTER_SANITIZE_EMAIL);
-	$user_Phone       = filter_var($_POST["userPhone"], FILTER_SANITIZE_STRING);
-	$user_Message     = filter_var($_POST["userMessage"], FILTER_SANITIZE_STRING);
-	
+	$user_Name1     = filter_var($_POST["userName1"], FILTER_SANITIZE_STRING);
+	$user_Email1    = filter_var($_POST["userEmail1"], FILTER_SANITIZE_EMAIL);
+	$user_Pass1     = filter_var($_POST["userPass1"], FILTER_SANITIZE_STRING);
+	$user_Date1     = filter_var($_POST["userDate1"], FILTER_SANITIZE_STRING);
+
 	//additional php validation
-	if(strlen($user_Name)<4) // If length is less than 4 it will throw an HTTP error.
+	if(strlen($user_Name1)<4) // If length is less than 4 it will throw an HTTP error.
 	{
 		$output = json_encode(array('type'=>'error', 'text' => 'Nombre corto o vacío'));
 		die($output);
 	}
-	if(!filter_var($user_Email, FILTER_VALIDATE_EMAIL)) //email validation
+	if(!filter_var($user_Email1, FILTER_VALIDATE_EMAIL)) //email validation
 	{
 		$output = json_encode(array('type'=>'error', 'text' => 'Ingrese un mail válido'));
 		die($output);
 	}
-	if(!is_numeric($user_Phone)) //check entered data is numbers
+	if(strlen($user_Pass1)<6) //check entered data is numbers
 	{
-		$output = json_encode(array('type'=>'error', 'text' => 'Debe ingresar sólo números'));
+		$output = json_encode(array('type'=>'error', 'text' => 'Debe ingresar mínimo 6 caracteres'));
 		die($output);
 	}
-	if(strlen($user_Phone)<6) //check entered data is numbers
+	if(strlen($user_Date1)<5) //check emtpy message
 	{
-		$output = json_encode(array('type'=>'error', 'text' => 'Debe ingresar mínimo 6 dígitos'));
-		die($output);
-	}
-	if(strlen($user_Message)<5) //check emtpy message
-	{
-		$output = json_encode(array('type'=>'error', 'text' => 'Mensaje muy corto'));
+		$output = json_encode(array('type'=>'error', 'text' => 'Seleccione fecha'));
 		die($output);
 	}
 	
-	//proceed with PHP email.
-	$headers = 'From: '.$user_Email.'' . "\r\n" .
-	'Reply-To: '.$user_Email.'' . "\r\n" .
-	'X-Mailer: PHP/' . phpversion();
-	
-	$sentMail = @mail($to_Email, $subject,'Nombre: '.$user_Name."\n"."\n".'Fono: '.$user_Phone."\n"."\n".'Mail: '.$user_Email."\n"."\n".'Mensaje: ' .$user_Message, $headers);
-	
-	if(!$sentMail)
-	{
-		$output = json_encode(array('type'=>'error', 'text' => 'No se pudo enviar el mensaje.'));
+	$SQL = "Select * From usuario Where usu_mail = '$user_Email1'";
+	$consulta = mysql_query($SQL);
+	$filas = mysql_num_rows($consulta);
+	$datos = mysql_fetch_row($consulta);
+
+	if ($filas>0){
+		$output = json_encode(array('type'=>'error', 'text' => 'Correo ya registrado'));
 		die($output);
 	}else{
-		$output = json_encode(array('type'=>'message', 'text' => 'Hola '.$user_Name .' Gracias por enviar su mensaje.'));
+
+	if(strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
+
+	
+		$SQL = "Insert Into usuario (usu_nombre, usu_mail, usu_pass, usu_edad) 
+	
+		Values ('".$user_Name1."','".$user_Email1."','".$user_Pass1."','".$user_Date1."')";
+		mysql_query($SQL);
+
+		$output = json_encode(array('type'=>'error', 'text' => 'Usuario registrado correctamente'));
 		die($output);
+
+	}else{
+		$output = json_encode(array('type'=>'error', 'text' => 'Error al registrar usuario'));
+		die($output);	}                               
 	}
+	
 }
-?>
-
-
-<?php
-	include 'conexion.php';
-	
-	$nombre = $_POST['nombre'];
-	$email = $_POST['email'];
-	$pass = md5($_POST['pass']);
-	$fono = $_POST['fono'];
-	$direccion = $_POST['direccion'];
-	$comuna = $_POST['comuna'];
-	$pagina = $_POST['pagina'];
-
-		
-		$SQL = "Select * From usuario Where usu_email = '$email'";
-		$consulta = mysql_query($SQL);
-		$filas = mysql_num_rows($consulta);
-		$datos = mysql_fetch_row($consulta);
-
-			if ($filas>0){
-				echo "<script>location.href='../$pagina.php?err=3';</script>";
-			}else{
-				$SQL = "Insert Into usuario (usu_nom, usu_tel, usu_dir, usu_com, usu_email, usu_pass, usu_tipo, usu_est) 
-				Values ('".$nombre."','".$fono."','".$direccion."','".$comuna."','".$email."','".$pass."',1,'inactivo')";
-				mysql_query($SQL);
-				mail($email'Comprobar email',
-				"Para comprobar su email pinche el siguiente link:\n\n".
-
-				"<a href=\"http://www.elpollitope.cl/funtion/comprobar.php?email=$email");
-
-				echo "<script>location.href='../$pagina.php?err=4';</script>";
-			}	
-	
 ?>
--- js/script.js
+++ js/script.js
@@ -63,32 +63,32 @@ $(document).ready(function() {
     });
     /* FIN SCRIPT CONTACT FORM*/
 
+    /* INICIO SCRIPT REGISTRO FORM*/
 
-    /*INICIO SCRIPT REGISTRO*/
-    $("#submit_btn").click(function() { 
+    $("#submit_btn1").click(function() { 
         //get input field values
-        var user_name       = $('input[name=name]').val(); 
-        var user_email      = $('input[name=email]').val();
-        var user_phone      = $('input[name=phone]').val();
-        var user_message    = $('textarea[name=message]').val();
+        var user_name1       = $('input[name=name1]').val(); 
+        var user_email1      = $('input[name=email1]').val();
+        var user_pass1      = $('input[name=pass1]').val();
+        var user_date1    = $('textarea[name=date1]').val();
         
         //simple validation at client's end
         //we simply change border color to red if empty field using .css()
         var proceed = true;
-        if(user_name==""){ 
-            $('input[name=name]').css('border-color','red'); 
+        if(user_name1==""){ 
+            $('input[name=name1]').css('border-color','red'); 
             proceed = false;
         }
-        if(user_email==""){ 
-            $('input[name=email]').css('border-color','red'); 
+        if(user_email1==""){ 
+            $('input[name=email1]').css('border-color','red'); 
             proceed = false;
         }
-        if(user_phone=="") {    
-            $('input[name=phone]').css('border-color','red'); 
+        if(user_pass1=="") {    
+            $('input[name=pass1]').css('border-color','red'); 
             proceed = false;
         }
-        if(user_message=="") {  
-            $('textarea[name=message]').css('border-color','red'); 
+        if(user_date1=="") {  
+            $('textarea[name=date1]').css('border-color','red'); 
             proceed = false;
         }
 
@@ -96,10 +96,10 @@ $(document).ready(function() {
         if(proceed) 
         {
             //data to be sent to server
-            post_data = {'userName':user_name, 'userEmail':user_email, 'userPhone':user_phone, 'userMessage':user_message};
+            post_data = {'userName1':user_name1, 'userEmail1':user_email1, 'userPass1':user_pass1, 'userDate1':user_date1};
             
             //Ajax post data to server
-            $.post('function/contacto.php', post_data, function(response){  
+            $.post('function/registro.php', post_data, function(response){  
 
                 //load json data from server and output message     
                 if(response.type == 'error')
@@ -109,20 +109,20 @@ $(document).ready(function() {
                     output = '<div class="success">'+response.text+'</div>';
                     
                     //reset values in all input fields
-                    $('#contact_form input').val(''); 
-                    $('#contact_form textarea').val(''); 
+                    $('#contact_form1 input').val(''); 
+                    $('#contact_form1 textarea').val(''); 
                 }
                 
-                $("#result").hide().html(output).slideDown();
+                $("#result1").hide().html(output).slideDown();
             }, 'json');
             
         }
     });
     
     //reset previously set border colors and hide all message on .keyup()
-    $("#contact_form input, #contact_form textarea").keyup(function() { 
-        $("#contact_form input, #contact_form textarea").css('border-color',''); 
-        $("#result").slideUp();
+    $("#contact_form1 input, #contact_form textarea").keyup(function() { 
+        $("#contact_form1 input, #contact_form textarea").css('border-color',''); 
+        $("#result1").slideUp();
     });
     /* FIN SCRIPT REGISTRO FORM*/
 

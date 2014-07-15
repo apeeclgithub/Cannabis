81f46aa1be5769a6c446dcdbf91a5a3dee9ddbb8
apeeclgithub <apeeclgithub@gmail.com>
2014-07-15 01:16:49 -0400

    update registro

--- js/script.js
+++ js/script.js
@@ -110,13 +110,15 @@ $(document).ready(function() {
 
  /* INICIO SCRIPT REGISTRO FORM*/
 function validarReg() {
+
+
     var name_err = email_err = pass_err = '';
     var name1 = document.getElementById("name1").value;
     var namexp = /[A-Z a-z]{3}/;
         if (namexp.test(name1)) {
             //alert('bien nombre');
         }else{
-            name_err = 'mal nombre';
+            name_err = 'Ingrese nombre';
         }
 
     var email1 = document.getElementById("email1").value;
@@ -124,7 +126,7 @@ function validarReg() {
         if (emailexp.test(email1)) {
             //alert('bien mail');
         }else{
-            email_err = 'mal mail';
+            email_err = 'Ingrese mail válido';
         }
 
 
@@ -133,11 +135,53 @@ function validarReg() {
         if (passexp.test(pass1)){
             //alert('bien pass');
         }else{
-            pass_err = 'mal pass';
+            pass_err = 'Ingrese contraseña';
+        }
+/* 
+    var day = document.getElementById('day').value;
+    var month = document.getElementById('month').value;
+    var year = document.getElementById('year').value;
+
+   function getAge(year, month, day) {
+   var today = new Date();
+   var birthDate = new Date(year, month, day);
+   var age = today.getFullYear() - birthDate.getFullYear();
+   var m = today.getMonth() - birthDate.getMonth();
+   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+       age--;
+   }
+   return age;
+}*/
+
+        if(name1==""){ 
+            $('input[name=name1]').css('border-color','red'); 
+            proceed = false;
+        }
+        if(email1==""){ 
+            $('input[name=email1]').css('border-color','red'); 
+            proceed = false;
+        }
+        if(pass1=="") {    
+            $('input[name=pass1]').css('border-color','red'); 
+            proceed = false;
         }
 
+
+    $("#regform input").keyup(function() { 
+        $("#regform input").css('border-color',''); 
+    });
+
+    /*   if (name1 == '' || email1 == '' || pass1 == '') {
+        $(".error_class").empty().html("Campos vacios").fadeIn(2000, function(){
+        $(this).fadeOut();
+      });
+        return false;
+    }
+*/
     if (name_err !== '' || email_err !== '' || pass_err !== '') {
-        alert(name_err + email_err + pass_err);
+        $(".error_class").empty().html(name_err+'<br>' + email_err +'<br>'+ pass_err+'<br><br>').fadeIn(2000, function(){
+        $(this).fadeOut();
+      });
         return false;
     }
 

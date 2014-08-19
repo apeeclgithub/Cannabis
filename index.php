3a920f744e1e0a29c1e34d52e8e0b86013925391
apeeclgithub <apeeclgithub@gmail.com>
2014-08-19 17:37:19 -0400

    css

--- index.php
+++ index.php
@@ -101,31 +101,33 @@
 				<div class="logo">
 					<h1><img src="img/evn-gif.gif" alt="Espacio verde natural"></h1>
 				</div>
-			<div class="sesion">
-				<a href="registro.php" id="menu_registro">REGISTRO</a>
-				<a href="cuenta.php" id="menu_cuenta">MI CUENTA</a>
-				<a href="#" id="img_carro" ><img src="img/carro.png" alt="carro"></a>
-				<div id="colorNav">
-					<ul>
-						<li>
-							<a href="">CARRO (<?php echo $carrito->articulos_total()?>)</a>
-							<?php
-								echo '<ul>';
-								$carro = $carrito->get_content();
-								if($carro){
-								    foreach($carro as $producto){
-								    	
-								        echo '<li><a href="#">'.$producto["cantidad"].' x '.$producto["nombre"].'</a></li>';
-								    }
-								    
-									echo "<li><a>Valor Total: CLP ".number_format($carrito->precio_total(),0,',','.')."</a></li>";
-									echo "<li><a><span onclick=\"delall('si')\">Borrar Todo</span></a></li>";
-									echo "<li><a href=\"venta.php\"><button class=\"comprar\">Modificar y Compra</button></a></li>";
-								}
-								echo '</ul>';
-							?>
-						</li>
-					</ul>
+			<div class="cont_sesion_top">
+				<div class="sesion">
+					<a href="registro.php" id="menu_registro">REGISTRO</a>
+					<a href="cuenta.php" id="menu_cuenta">MI CUENTA</a>
+					<a href="#" id="img_carro" ><img src="img/carro.png" alt="carro"></a>
+					<div id="colorNav">
+						<ul>
+							<li>
+								<a href="">CARRO (<?php echo $carrito->articulos_total()?>)</a>
+								<?php
+									echo '<ul>';
+									$carro = $carrito->get_content();
+									if($carro){
+									    foreach($carro as $producto){
+									    	
+									        echo '<li><a href="#">'.$producto["cantidad"].' x '.$producto["nombre"].'</a></li>';
+									    }
+									    
+										echo "<li><a>Valor Total: CLP ".number_format($carrito->precio_total(),0,',','.')."</a></li>";
+										echo "<li><a><span onclick=\"delall('si')\">Borrar Todo</span></a></li>";
+										echo "<li><a href=\"venta.php\"><button class=\"comprar\">Modificar y Compra</button></a></li>";
+									}
+									echo '</ul>';
+								?>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</header>

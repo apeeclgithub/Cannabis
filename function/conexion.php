64f060bc2094c8d34c6a28e4a964b2c0b2dca924
apeeclgithub <apeeclgithub@gmail.com>
2014-08-21 18:20:59 -0400

    conexios

--- function/conexion.php
+++ function/conexion.php
@@ -1,11 +1,11 @@
 <?php
 
-$con= mysql_connect("localhost","root","");
-//$con= mysql_connect("apeecl_evnprueba","apeecl_evnprueba","C{ig+7zSyieT");
+//$con= mysql_connect("localhost","root","");
+$con= mysql_connect("apeecl_evnprueba","apeecl_evnprueba","n&ZCMz[4Zdp^");
 mysql_query("SET NAMES 'utf8'");
 if($con) 
 {   
-	$db = mysql_select_db("cannabis",$con);
+	$db = mysql_select_db("apeecl_evnprueba",$con);
 	if(!$db)
 	{ 
        		echo "Problemas para conectar a la BD";

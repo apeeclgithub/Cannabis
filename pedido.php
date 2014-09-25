d3de8761e77698d09c88053333ad388d9818a9ef
apeeclgithub <apeeclgithub@gmail.com>
2014-09-25 14:32:53 -0300

    css

--- pedido.php
+++ pedido.php
@@ -1,3 +1,7 @@
+<?php
+    include 'function/carrito.php';
+    include 'function/conexion.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -36,10 +40,81 @@
     <meta name="keywords" content="venta cremas, venta semillas, semillas, cremas, ropa, vestuario, venta vestuario,  venta ropa, venta fertilizante, fertilizante, cosmetica, semillas feminizadas, feminizadas, semillas automaticas, automaticas, accesorios, vestuario y accesorios, marihuana, cannabis">
 </head>
 	<body>
-
+        <header class="header">
+            <div class="logo">
+                <h1><img src="img/evn-gif.gif" alt="Espacio verde natural"></h1>
+            </div>
+            <div class="cont_color_follow">
+                <div class="cont_follow_top">
+                    <div class="follow_top">
+                        <div class="sig_top">
+                            SÍGUENOS
+                        </div>
+                        <div class="redestop"><a href="http://www.twitter.com" target="_blank"><img src="img/twitter.png" alt="Twitter"></a><a href="http://www.instagram.com" target="_blank"><img src="img/instagram.png" alt="Instagram"></a><a href="http://www.facebook.com" target="_blank"><img src="img/facebook.png" alt="Facebook"></a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="ul_menu">
+                <nav class="menu_p">
+                    <div id="menuGrande">
+                        <ul>
+                            <li ><a href="index.php" id="menu_inicio">INICIO</a></li>
+                            <li ><a href="quienes-somos.php" id="menu_quienes">QUIÉNES SOMOS</a></li>
+                            <li ><a href="nuestros-productos.php" id="menu_prod">NUESTROS PRODUCTOS</a></li>
+                            <li ><a href="contacto.php"     id="menu_cont">CONTACTO</a></li>
+                            <li ><a href="condiciones.php" id="menu_cond">CONDICIONES DE COMPRA</a></li>
+                            <li ><a href="politicas.php" id="menu_pol">POLÍTICAS DE PRIVACIDAD Y TÉRMINOS DE USO</a></li>
+                        </ul>
+                    </div>
+                    <div id="menuChico">
+                    <input id="check1" type="checkbox" />
+                    <label for="check1">
+                        <img src="img/menu.png" alt="icono">
+                    </label>
+                    <div class="menu">
+                        <ul>
+                            <li ><a href="index.php" >INICIO</a></li>
+                            <li ><a href="quienes-somos.php" >QUIÉNES SOMOS</a></li>
+                            <li ><a href="nuestros-productos.php" >NUESTROS PRODUCTOS</a></li>
+                            <li ><a href="contacto.php"     >CONTACTO</a></li>
+                            <li ><a href="condiciones.php" >CONDICIONES DE COMPRA</a></li>
+                            <li ><a href="politicas.php" >POLÍTICAS DE PRIVACIDAD Y TÉRMINOS DE USO</a></li>
+                        </ul>
+                    </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="cont_sesion_top">
+                <div class="sesion">
+                    <a href="registro.php" id="menu_registro">REGISTRO</a>
+                    <a href="cuenta.php" id="menu_cuenta">MI CUENTA</a>
+                    <a href="#" id="img_carro" ><img src="img/carro.png" alt="carro"></a>
+                    <div id="colorNav">
+                        <ul>
+                            <li>
+                                <a href="">CARRO (<?php echo $carrito->articulos_total()?>)</a>
+                                <?php
+                                    echo '<ul>';
+                                    $carro = $carrito->get_content();
+                                    if($carro){
+                                        foreach($carro as $producto){
+                                            
+                                            echo '<li><a href="#">'.$producto["cantidad"].' x '.$producto["nombre"].'</a></li>';
+                                        }
+                                        
+                                        echo "<li><a>Valor Total: CLP ".number_format($carrito->precio_total(),0,',','.')."</a></li>";
+                                        echo "<li><a><span onclick=\"delall('si')\">Borrar Todo</span></a></li>";
+                                        echo "<li><a href=\"pedido.php\"><button class=\"comprar\">Comprar</button></a></li>";
+                                    }
+                                    echo '</ul>';
+                                ?>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
 			<?php
-				include 'function/conexion.php';
-				include 'function/carrito.php'; 
 				$carro = $carrito->get_content();
 						if($carro){
 							echo "<h3>Lista de productos</h3>";
@@ -123,7 +198,36 @@
 		    	echo '<a href="inicio-sesion.php">Login</a>';
 		    }
 	    ?>
-
+        <footer class="pie">
+                <div class="cont_pie">
+                    <div class="logo_pie">
+                        <img src="img/logo-footer.png" alt="Espacio verde natural">
+                    </div>
+                    <div class="follow_pie">
+                        <div class="follow_text">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias</div>
+                        <div class="sig_foot">
+                            SÍGUENOS
+                        </div>
+                        <div class="tw_pie"><a href=""><img src="img/twitter.png" alt="Twitter"></a><a href=""><img src="img/instagram.png" alt="Instagram"></a><a href=""><img src="img/facebook.png" alt="Facebook"></a></div>
+                    </div>
+                    <div class="soporte">
+                        <div class="title_sop">SOPORTE</div>
+                        <div class="cond_sop">Condiciones de compra</div>
+                        <div class="cond_sop">Políticas de privacidad</div>
+                        <div class="cond_sop">Términos de uso</div>
+                        <div class="cond_sop_1">Preguntas frecuentes</div>
+                    </div>
+                    <div class="contacto">
+                        <div itemscope itemtype="http://schema.org/LocalBusiness">
+                            <div class="title_cont">CONTÁCTANOS</div>
+                            <div class="cont_mail"><img src="img/mail.png" alt="Correo">&nbsp;<span itemprop="email">ventas@espacioverdenatural.cl</span></div>
+                            <div class="cont_mail"><img src="img/telefono.png" alt="Telefono">&nbsp;&nbsp;&nbsp;&nbsp;<span itemprop="telephone">02/26995942</span></div>
+                            <div class="cont_mail"><img src="img/celu.png" alt="Celular">&nbsp;&nbsp;&nbsp;&nbsp;<span itemprop="telephone">6-1400225 / 6-8358062</span></div>                      
+                            <div class="cont_mail"><span itemprop="streetAddress">Huérfanos 1044</span>, oficina 905, <span itemprop="addressLocality">Santiago Centro.</span></div>                      
+                        </div>
+                    </div>
+                </div>
+        </footer>  
 
 
 	</body>	

6e6be36b347b3940aeeb1fffcb4e5f2a1c9c6857
pabloinginformatica <pablo.ing.infotmatica@gmail.com>
2014-11-08 02:34:57 -0400

    zsd

--- ver_detalle.php
+++ ver_detalle.php
@@ -19,21 +19,42 @@
 <body>
 
 	<?php  
-		include 'function/conexion.php';
+
+    include 'function/conexion.php';
 
 		$id = $_GET["id"];
 
-		$sql = "SELECT pro_nombre, pro_valor FROM detalle INNER JOIN producto ON detalle.pro_id = producto.pro_id WHERE ped_id = ".$id;
-		$con = mysql_query($sql);
-		//$num = mysql_num_rows($con);
-		//$num = mysql_fetch_array($con);
-		$arr = mysql_fetch_assoc($con);
 
-		while ($num = mysql_fetch_array($con)) {
-			echo $num['pro_nombre'];
-		}
+
+                        $sql = "SELECT ped_id, ped_valor, ped_estado FROM pedido WHERE ped_id = ".$id;
+                        $con = mysql_query($sql);
+                        $dato = mysql_fetch_array($con);
+                        if(empty($dato)){
+                            echo "<div class=\"cont_dato_nocompra\">No ha realizado compras</div>";
+                        }else{
+                        do  {
+                            echo "<ul><br>";
+                            echo "<li class=\"dropdown\">Número de pedido: ".$dato['ped_id']."<br> Valor: ".$dato['ped_valor']."<br> Estado Actual: ".$dato['ped_estado']."<br>";
+                            $sql2 = "SELECT pro_nombre, det_cantidad, pro_valor FROM detalle INNER JOIN producto ON detalle.pro_id = producto.pro_id WHERE ped_id = ".$dato['ped_id'];
+                            $con2 = mysql_query($sql2);
+                            $dato2 = mysql_fetch_array($con2);
+                            do {
+                                echo "<ul>";
+                                echo "<li><br>".$dato2['pro_nombre']."<br>";
+                                echo "Cantidad: ".$dato2['det_cantidad']."<br>";
+                                echo "Valor: ".$dato2['pro_valor']."<br></li>";
+                                echo "</ul>";
+
+                            } while ($dato2 = mysql_fetch_array($con2));
+                            echo "</li>";
+                            echo "</ul>";
+                            } while ($dato = mysql_fetch_array($con));
+                            echo "<a href='function/aprobar.php?id=".$id."'>APROBAR</a> / <a href='function/rechazar.php?id=".$id."'>RECHAZAR</a><br><br>";
+                        }
 
 	?>
+    
+    <input type="button" value="VOLVER ATRÁS" name="Back2" onclick="history.back()" />
 	
 </body>
 </html>

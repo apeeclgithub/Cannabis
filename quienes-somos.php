619b7fc20d560061b8c7caab9fb7988bbeeae402
apeeclgithub <apeeclgithub@gmail.com>
2015-03-23 17:17:24 -0300

    strong

--- quienes-somos.php
+++ quienes-somos.php
@@ -121,7 +121,7 @@
             <article >
                 <div class="title_condic"><h2>QUIÉNES SOMOS</h2></div>
                 <div class="texto_condic">
-                    Espacio Verde Natural, es una empresa de jóvenes profesionales que gusta de la vida sana y natural, que está ubicada en Santiago de Chile. Surge luego de un intenso año de trabajo con el propósito de dar a conocer sobre la utilización de las conocidas semillas de <strong class="seo">cáñamo<strong> y sus derivados.<br><br>
+                    Espacio Verde Natural, es una empresa de jóvenes profesionales que gusta de la vida sana y natural, que está ubicada en Santiago de Chile. Surge luego de un intenso año de trabajo con el propósito de dar a conocer sobre la utilización de las conocidas semillas de <strong class="seo">cáñamo</strong> y sus derivados.<br><br>
                     Además, no olvidemos las aplicaciones terapéuticas y cosméticas de estas semillas y que cada vez están siendo más utilizadas, principalmente en las áreas de salud, algunas de estas variedades se encuentran en nuestro catálogo.<br><br>
                     Somos un equipo con una gran empatía y con valores bien definidos, donde nuestra principal prioridad es la satisfacción de nuestros clientes. Creemos en lo que hacemos por eso entendemos los negocios como una relación basada en la confianza con nuestros clientes, donde la satisfacción y la calidad sean los principales pilares.<br><br>
                     Los productos que encontrarán en esta web son de marcas de referencia en el ámbito del cultivo y la cosmética, y tienen todas las garantías para satisfacer tus expectativas. Enviamos nuestros productos a cualquier parte del país. Te invitamos a vivir esta experiencia con nosotros.<br><br>
